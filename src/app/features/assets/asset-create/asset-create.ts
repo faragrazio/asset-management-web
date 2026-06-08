483844d4c545ec9ee8e8ce7bb43246d6288249ae
Graziano Faraone <grazianofaraone@gmail.com>
2026-06-08 18:20:20 +0200

    feat: add required validation to create form

--- src/app/features/assets/asset-create/asset-create.ts
+++ src/app/features/assets/asset-create/asset-create.ts
@@ -1,11 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { AssetService } from '../../../core/services/asset.service';
 
 @Component({
   selector: 'app-asset-create',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './asset-create.html',
   styleUrl: './asset-create.scss',
 })
@@ -16,16 +16,20 @@ export class AssetCreate {
 
   // nonNullable: i campi avranno sempre un testo (al massimo vuoto), mai "null"
   readonly form = this.fb.nonNullable.group({
-    name: [''],
-    serialNumber: [''],
-    category: [''],
-    location: [''],
-    purchaseDate: [''],
+    name: ['', Validators.required],
+    serialNumber: ['', Validators.required],
+    category: ['', Validators.required],
+    location: ['', Validators.required],
+    purchaseDate: ['', Validators.required],
   });
 
   readonly error = signal<string | null>(null);
 
   onSubmit(): void {
+     if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
     this.assetService.create(this.form.getRawValue()).subscribe({
       next: () => {
         // Salvataggio riuscito: torniamo alla lista
